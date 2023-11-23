258e0389443ac5ad1d9aff820b5581dcd0a4c445
Nojotel <nojotel@yandex.ru>
2023-11-23 15:31:08 +0300

    /
    /

---
+++ js/app.js
@@ -0,0 +1,80 @@
+const buttonКequestСonsultation = document.querySelectorAll(".offer-button");
+const containerКequestСonsultation = document.querySelector(".container-section__three");
+const buttonSendForm = document.querySelector(".section__three-button");
+const formName = document.querySelector("#name");
+const formTel = document.querySelector("#phone");
+const formMail = document.querySelector("#email");
+const containeExperts = document.querySelector(".section__four-container__experts");
+
+let experts = [
+  {
+    url: "../img/Experts1.png",
+  },
+  {
+    url: "../img/Experts2.png",
+  },
+  {
+    url: "../img/Experts3.png",
+  },
+  {
+    url: "../img/Experts4.png",
+  },
+  {
+    url: "../img/Experts5.png",
+  },
+  {
+    url: "../img/Experts6.png",
+  },
+  {
+    url: "../img/Experts7.png",
+  },
+  {
+    url: "../img/Experts8.png",
+  },
+];
+
+buttonКequestСonsultation.forEach((button) => {
+  button.addEventListener("click", function () {
+    containerКequestСonsultation.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+buttonSendForm.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append("name", formName.value);
+  formData.append("phone", formTel.value);
+  formData.append("email", formMail.value);
+
+  fetch("", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Form submitted successfully:", data);
+    })
+    .catch((error) => {
+      console.error("Error submitting form:", error);
+    });
+});
+
+experts.forEach((expert, index) => {
+  // Create a div element with a specific class
+  const divElement = document.createElement("div");
+  divElement.classList.add("expert-container");
+
+  // Create an img element
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("expert-container__img");
+
+  // Set the src attribute of the img element
+  imgElement.src = expert.url;
+
+  // Append the img element to the div element
+  divElement.appendChild(imgElement);
+
+  // Append the div element to the container
+  containeExperts.appendChild(divElement);
+});
