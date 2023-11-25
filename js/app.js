dd20c09ac99b9599c630317e5a7f42c508679d80
Nojotel <nojotel@yandex.ru>
2023-11-26 02:12:14 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,10 +6,49 @@ const formTel = document.querySelector("#phone");
 const formMail = document.querySelector("#email");
 const containeExperts = document.querySelector(".section__four-container__experts");
 
-const experts = [{ url: "./img/Experts1.png" }, { url: "./img/Experts2.png" }, { url: "./img/Experts3.png" }, { url: "./img/Experts4.png" }, { url: "./img/Experts5.png" }, { url: "./img/Experts6.png" }, { url: "./img/Experts7.png" }, { url: "./img/Experts8.png" }];
+let experts = [
+  {
+    url: "./img/Experts1.png",
+  },
+  {
+    url: "./img/Experts2.png",
+  },
+  {
+    url: "./img/Experts3.png",
+  },
+  {
+    url: "./img/Experts4.png",
+  },
+  {
+    url: "./img/Experts5.png",
+  },
+  {
+    url: "./img/Experts6.png",
+  },
+  {
+    url: "./img/Experts7.png",
+  },
+  {
+    url: "./img/Experts8.png",
+  },
+];
+
+experts.forEach((expert, index) => {
+  const divElement = document.createElement("div");
+  divElement.classList.add("expert-container");
+
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("expert-container__img");
+
+  imgElement.src = expert.url;
+
+  divElement.appendChild(imgElement);
+
+  containeExperts.appendChild(divElement);
+});
 
 buttonКequestСonsultation.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", function () {
     containerКequestСonsultation.scrollIntoView({ behavior: "smooth" });
   });
 });
